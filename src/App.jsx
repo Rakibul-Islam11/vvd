427572fa15de4bba43d67f6b5faad0880d301f53
rakib <kathgolap909@gmail.com>
2024-12-28 16:02:03 +0600

    jhi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import vdo from './assets/videos/sdsd.mp4';
-import imgg from './assets/img/gg.png'
+import imgg from './assets/img/gg.png';
 import './App.css';
 
 function App() {
@@ -12,7 +12,16 @@ function App() {
 
   return (
     <>
-      <div className="video-container" style={{ position: 'relative' }}>
+      <div
+        className="video-container"
+        style={{
+          position: 'relative',
+          width: '100%',
+          maxWidth: '800px', // Optional, যদি একটি নির্দিষ্ট প্রস্থ দিতে চান
+          aspectRatio: '16 / 9', // ভিডিওর অনুপাত ধরে রাখার জন্য
+          overflow: 'hidden',
+        }}
+      >
         {/* কভার ইমেজ */}
         {!isVideoLoaded && (
           <img
@@ -37,12 +46,24 @@ function App() {
           autoPlay
           onLoadedData={handleVideoLoaded}
           style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
             width: '100%',
             height: '100%',
+            objectFit: 'cover',
             display: isVideoLoaded ? 'block' : 'none',
           }}
         ></video>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quia nesciunt ducimus recusandae, atque accusantium. At tempore ea quod porro! Quasi eligendi corrupti asperiores nesciunt nostrum impedit sint, officiis reprehenderit.</p>
+        <p
+          style={{
+            marginTop: '16px',
+            fontSize: '16px',
+            textAlign: 'center',
+          }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quia nesciunt ducimus recusandae, atque accusantium. At tempore ea quod porro! Quasi eligendi corrupti asperiores nesciunt nostrum impedit sint, officiis reprehenderit.
+        </p>
       </div>
     </>
   );
