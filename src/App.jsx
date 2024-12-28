bb7b815ccdc4a25f3957938927a1688575b9263a
rakib <kathgolap909@gmail.com>
2024-12-28 14:59:49 +0600

    nuh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
-
+import { useState } from 'react';
+import vdo from './assets/videos/coverVideo.mp4';
+import imgg from './assets/img/gg.png'
 import './App.css';
 
 function App() {
-  
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
+  const handleVideoLoaded = () => {
+    setIsVideoLoaded(true);
+  };
+
   return (
     <>
-     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid nostrum velit fugit consequatur sint ut officia architecto eaque repudiandae amet, facilis voluptatibus magnam, quidem possimus soluta reiciendis eveniet suscipit. Vero!</p>
+      <div className="video-container" style={{ position: 'relative' }}>
+        {/* কভার ইমেজ */}
+        {!isVideoLoaded && (
+          <img
+            src={imgg}
+            alt="Cover"
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              zIndex: 1,
+            }}
+          />
+        )}
+
+        {/* ভিডিও */}
+        <video
+          src={vdo}
+          muted
+          autoPlay
+          onLoadedData={handleVideoLoaded}
+          style={{
+            width: '100%',
+            height: '100%',
+            display: isVideoLoaded ? 'block' : 'none',
+          }}
+        ></video>
+      </div>
     </>
   );
 }
